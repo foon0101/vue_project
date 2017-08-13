f0b55f0ae26cb52a2587cc223d50758a127e6c90
foon0101 <klqin0101@163.com>
2017-08-13 16:04:44 +0800

    完成首页轮播图和九宫格&&搭建新闻页面文件

---
+++ src/component/index/swipe.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="swipe">
+        <mt-swipe :auto="2000">
+            <mt-swipe-item v-for="(item,i) in list">
+                <a :href="item.url">
+                    <img :src="item.img" />
+                </a>
+            </mt-swipe-item>
+        </mt-swipe>
+    </section>
+</template>
+
+<script>
+    export default {
+
+        // 定义该组件所有的数据
+        data(){
+            return{
+                // 轮播图列表数据
+                list:[]
+            };
+        },
+
+        // 定义该组件所有的逻辑方法
+        methods:{
+            // 互殴轮播图数据的方法
+            getLunbo(){
+                let url = "http://139.199.192.48:8888/api/getlunbo";
+                this.$http.get(url).then(resp => {
+                    if(resp.body.status == 0){
+                        this.list = resp.body.message;
+                    }
+                });
+            }
+        },
+        // 当组件初始化完毕，数据和事件都绑定好之后自动执行
+        created(){
+            this.getLunbo();
+        }
+    }
+</script>
+
+<style lang="less">
+    .swipe {
+        height:300px;
+        img{
+            width:100%;
+            height:100%;
+        }
+    }
+</style>
---
+++ src/component/news/list.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="details">
+    
+  </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="less">
+
+</style>
